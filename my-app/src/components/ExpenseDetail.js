4a283e947238e7d248f6194266524abceedb84c2
Sachinshekharpatel <heroft7024@gmail.com>
2024-01-15 13:57:54 +0530

    DeleteFromDOmWorking Using addeventlistener ion jsx

--- my-app/src/components/ExpenseDetail.js
+++ my-app/src/components/ExpenseDetail.js
@@ -4,12 +4,21 @@ let ExpenseDetail = (props) =>{
     let title = props.title
     let amount =  props.amount;
     let location = props.location;
+    let deletebtnhandler = (e) => {
+           console.log("Deleted")
+          //  let parent = e.target.closest('.expense-item__description');
+           let parent = e.target.parentNode;
+           let parentElement = parent.parentNode;
+           let main = parentElement.parentNode;
+           main.removeChild(parentElement);
+    }
+
     return (
         <div className="expense-item__description">
           <h2>{title}</h2>
           <h2>{location}</h2>
           <h2 className='expense-item__price'>Rs {amount}</h2>
-         
+          <button onClick={deletebtnhandler}>Delete Button</button>
         </div>
 
     )
