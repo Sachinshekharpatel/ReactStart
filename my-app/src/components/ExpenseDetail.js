8a940e2b6c77437236136b197b4d2f1fcd7c776f
Sachinshekharpatel <heroft7024@gmail.com>
2024-01-16 13:50:43 +0530

    Add expensebtn added

--- my-app/src/components/ExpenseDetail.js
+++ my-app/src/components/ExpenseDetail.js
@@ -1,24 +1,35 @@
 import './ExpenseDate.css';
+import React, {useState} from 'react';
 
 let ExpenseDetail = (props) =>{
-    let title = props.title
+    let title = props.title ;
+    let [expense,setExpense] = useState(props.amount)
     let amount =  props.amount;
     let location = props.location;
+
     let deletebtnhandler = (e) => {
            console.log("Deleted")
-          //  let parent = e.target.closest('.expense-item__description');
+          //  title = "updated";
+          // console.log(title)
+         
            let parent = e.target.parentNode;
            let parentElement = parent.parentNode;
            let main = parentElement.parentNode;
            main.removeChild(parentElement);
+    }
+    
+    let expenseUpdated = () => {
+      setExpense('100.00');
+      // console.log(expense)
     }
 
     return (
         <div className="expense-item__description">
           <h2>{title}</h2>
           <h2>{location}</h2>
-          <h2 className='expense-item__price'>Rs {amount}</h2>
+          <h2 className='expense-item__price'>Rs {expense}</h2>
           <button onClick={deletebtnhandler}>Delete Button</button>
+          <button onClick={expenseUpdated}>Expense Updated</button>
         </div>
 
     )
