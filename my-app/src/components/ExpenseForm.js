13ca37202d171f1b127cd63191f95700adfca022
Sachinshekharpatel <heroft7024@gmail.com>
2024-01-16 16:58:25 +0530

    Userdetail are now showing in console when we click on the  submit button of formtag

--- my-app/src/components/ExpenseForm.js
+++ my-app/src/components/ExpenseForm.js
@@ -6,16 +6,32 @@ function ExpenseForm(){
      const [amount,setAmount] = useState('');
      const [date,setDate] = useState('');
 
-
+    //   const [Input ,setInput] = useState({
+    //     title : '',
+    //     amount : '',
+    //     dtae : ''
+    //   })
     let changetitle = (e) =>{
        e.preventDefault();
        setTitle(e.target.value);
+    //    setInput((previousdata)=>{
+    //     return ({
+    //         ...previousdata,
+    //         title : e.target.value
+    //     })
+    //    })
     }
     
      
     let changeamount = (e) =>{
          e.preventDefault();
          setAmount(e.target.value);
+        // setInput((previousdata)=>{
+        //     return ({
+        //         ...previousdata,
+        //         amount : e.target.value
+        //     })
+        //    })
     }
 
     
@@ -23,18 +39,41 @@ function ExpenseForm(){
     let changedate = (e) =>{
          e.preventDefault();
          setDate(e.target.value);
+        // setInput((previousdata)=>{
+        //     return ({
+        //         ...previousdata,
+        //         date : e.target.value
+        //     })
+        //    })
+    }
+    let obj = {
+        title : "",
+        amount : "",
+        date : '',
+    };
+    let Userdetail = (e) =>{
+        e.preventDefault()
+        let titlevalue  = document.querySelector('.mytitle').value;
+        let datevalue  = document.querySelector('.mydate').value;
+        let amountvalue  = document.querySelector('.myamount').value;
+        
+        obj.title = titlevalue;
+        obj.date = datevalue ;
+        obj.amount = amountvalue ;
+        console.log(obj)
+
     }
 
     return (
     <div className="Formdiv">
         <form className="myform">
             <label>Title : </label>
-            <input type='text' className='mytitle' onChange={changetitle}></input>
+            <input type='text' className='mytitle' placeholder="Enter Title" onChange={changetitle}></input>
             <label>Amount : </label>
-            <input type='number' className='myamount' onChange={changeamount}></input>
+            <input type='number' className='myamount' placeholder="Enter Amount" onChange={changeamount}></input>
             <label>Date : </label>
-            <input type='date' className='mydate' onChange={changedate}></input>
-            <input type="submit" value="Submit"/>
+            <input type='date' className='mydate' min='2000-01-01' max='2024-01-15' onChange={changedate}></input>
+            <input type="submit" value="Submit" onClick={Userdetail}/>
         </form>
     </div>
 
